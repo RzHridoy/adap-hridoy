80432263b6a7665caf5eb046b9e03dd050045dae
RzHridoy <rz.hridoy003@gmail.com>
2025-11-23 13:06:09 +0100

    b03

--- src/adap-b03/names/StringName.ts
+++ src/adap-b03/names/StringName.ts
@@ -6,66 +6,99 @@ export class StringName extends AbstractName {
 
     protected name: string = "";
     protected noComponents: number = 0;
+    buildString: any;
+    parseComponents: any;
 
     constructor(source: string, delimiter?: string) {
         super();
-        throw new Error("needs implementation or deletion");
+        this.name = source ?? "";
+        this.noComponents = this.parseComponents().length;
     }
 
     public clone(): Name {
-        throw new Error("needs implementation or deletion");
+        return new StringName(this.name, this.delimiter);
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        if (delimiter === this.delimiter) {
+            return this.name;
+        }
+        const components = this.parseComponents();
+        return this.buildString(components, delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        return this.name;
     }
 
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
+        return super.isEqual(other);
     }
 
     public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
+        return super.getHashCode();
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return super.isEmpty();
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return super.getDelimiterCharacter();
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.noComponents;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        if (i < 0 || i >= components.length) {
+            throw new RangeError("Index is over ranged");
+        }
+        return components[i];
     }
 
     public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        if (i < 0 || i >= components.length) {
+            throw new RangeError("Index is over ranged");
+        }
+        components[i] = c;
+        this.updateFromComponents(components);
     }
 
     public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        if (i < 0 || i >= components.length) {
+            throw new RangeError("Index is over ranged");
+        }
+        components.splice(i, 0, c);
+        this.updateFromComponents(components);
     }
 
     public append(c: string) {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        components.push(c);
+        this.updateFromComponents(components);
     }
 
     public remove(i: number) {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        if (i < 0 || i >= components.length) {
+            throw new RangeError("Index is over ranged");
+        }
+        components.splice(i, 1);
+        this.updateFromComponents(components);
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        super.concat(other);
+    }
+
+    protected updateFromComponents(components: string[]): void {
+        this.noComponents = components.length;
+        this.name = this.buildString(components, this.delimiter);
     }
 
 }
